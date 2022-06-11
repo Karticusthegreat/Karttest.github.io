6a96a4d1d14ff70f35fbd2dc89c7f9aeb747528e
Karticusthegreat <107299467+Karticusthegreat@users.noreply.github.com>
2022-06-11 16:13:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// this is javascript
+console.log("hello world");
+
+
+document.getElementById("btn2").onclick = function() {
+document.getElementById("text").style.fontSize = "40px"
+}
+document.getElementById("btn3").onclick = function() {
+document.getElementById("text").style.fontSize = "10px"
+}
+
+function dookie() {
+  var p = document.getElementById("paragraph"); p.style.display = "block";
+  console.log("you shat in your pants");
+}
